f87514542471f7f26e534535e00ac8ef2f9f1ce9
Christian Holtsclaw <ultragamerh21@gmail.com>
2018-06-29 11:01:23 -0400

    added factorials without recursion

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function add(x1,x2){
     return x1+x2;
 }
 
+//I only used recursion here to ensure that multiplying
+//by two negatives could be done much easier
 function multiply(x1,x2){
     var total=x1;
     if(x2<0&&x1<0)
@@ -29,11 +31,22 @@ function power(base,pow){
     return total;
 }
 
+//Factorial with recursion
 function factorial(n){
     if(n>0)
         return multiply(n,factorial(add(n,-1)));
 }
 
+//Factorial done with no recursion
+function factorialNoRecursion(n){
+    var total=n;
+    for(let i=2;i<n;i++){
+        total=multiply(total,i);
+    }
+    return total;
+}
+
+//Fibonacci with recursion
 function fibonacci(n){
     if(n<=1)
         return 0;
@@ -44,21 +57,13 @@ function fibonacci(n){
     return add(fibonacci(add(n,-1)),fibonacci(add(n,-2)));
 }
 
-function fib(n){
+//Fibinacci without recusrsion
+function fibonacciNoRecursion(n){
     var totals=[0,1];
     for(let i=1;i<n;i++){
         totals.push(add(totals[i],totals[i-1]))
     }
     return totals[add(n,-1)];
-}
-
-function fibArr(n){
-    var totals=[0,1];
-    for(let i=1;i<n;i++){
-        totals.push(totals[i]+totals[i-1])
-        
-    }
-    return totals;
 }
 
 function text(title,text){
@@ -71,5 +76,5 @@ function text(title,text){
 text('Add funtion 2+4',add(2,4));
 text('Multiply function 6*8',multiply(6,8));
 text('Power 2^8',power(2,8));
-text('Factorial 4!',factorial(4));
-text('Fibonacci 8', fib(8));
+text('Factorial 4!',factorialNoRecursion(4));
+text('Fibonacci 8', fibonacciNoRecursion(8));
